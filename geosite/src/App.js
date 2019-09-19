41400efb5c987f1d31ca7485a7b36d1edcb15794
Chad Archila <chad@Chads-MacBook-Pro.local>
2019-09-19 12:09:54 -0700

    Import marker, location data, update default center. Add custom marker func, style map component

--- geosite/src/App.js
+++ geosite/src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import {GoogleMap, withScriptjs, withGoogleMap} from 'react-google-maps'
+import {GoogleMap, withScriptjs, withGoogleMap, Marker} from 'react-google-maps';
+import * as locationData from './data/locationData.json';
 
 function Map (){
   return(
-<GoogleMap defaultZoom={13} defaultCenter={{lat: 37.543829, lng: -122.009618}}/> 
+    <GoogleMap 
+      defaultZoom={13} 
+      defaultCenter={{lat: 37.543829, lng: -122.009618}}
+    > 
 
+    {locationData.features.map((location) => (
+      <Marker
+        key={location.properties.LOCATION_ID}
+        position={{
+          lat: location.geometry.coordinates[1],
+          lng: location.geometry.coordinates[0]
+        }}
+      />
+    ))}
 
-  )
+    </GoogleMap>
+  );
 }
 
 const WrappedMap = withScriptjs(withGoogleMap(Map));
 
 export default function App() {
   return  (
-  <div style = {{width: '1000px', height: '1000px'}}> 
-    <WrappedMap googleMapURL = {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBpop_xGnr_52PJSdB4R_6tpEjqFlkdZtw`}
-    loadingElement = {<div style ={{height: "100%" }} />}
-    containerElement={<div style={{ height: "100%" }} />}
-    mapElement={<div style={{ height: "100%" }} />}
-  />  
-  </div>
+    <div style = {{width: '1000px', height: '1000px'}}> 
+      <WrappedMap googleMapURL = {`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyBpop_xGnr_52PJSdB4R_6tpEjqFlkdZtw`}
+      loadingElement = {<div style ={{height: "100%" }} />}
+      containerElement={<div style={{ height: "100%" }} />}
+      mapElement={<div style={{ height: "100%" }} />}
+      />  
+    </div>
   )
 } 
